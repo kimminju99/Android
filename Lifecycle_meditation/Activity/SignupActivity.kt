9b504a3bd1fda1348da368ef79ad203552aac955
kimminju99 <50831854+kimminju99@users.noreply.github.com>
2021-02-05 02:34:08 +0900

    Log in 창에서 sign up 갈 수 있게 수정
    signup id 수정

--- Lifecycle_meditation/Activity/SignupActivity.kt
+++ Lifecycle_meditation/Activity/SignupActivity.kt
@@ -1,33 +1,29 @@
 package com.example.lifecycle_meditation
 
-import android.R
-import android.graphics.Color
-import android.graphics.Typeface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
-import android.text.style.RelativeSizeSpan
-import android.text.style.StyleSpan
 import android.util.Log
 import android.view.View
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.example.lifecycle_meditation.databinding.ActivitySignupBinding
+import com.example.lifecycle_meditation.databinding.ActivityLoginBinding
 
+class LoginActivity : AppCompatActivity() {
 
-class SignupActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityLoginBinding
     val TAG: String = "로그"
-    private lateinit var binding: ActivitySignupBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG,"SignupActivity - onCreate() called")
+        Log.d(TAG,"LoginActivity - onCreate() called")
 
-        binding = ActivitySignupBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
         val view = binding.root
         setContentView(view)
+
+        binding.signBtnInLogin.setOnClickListener{
+            onSignupButtonClicked(view)
+        }
 
         binding.btnBack.setOnClickListener {
             onBackButtonClicked(view)
@@ -38,38 +34,43 @@ class SignupActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,"SignupActivity - onStart() called")
+        Log.d(TAG,"LoginActivity - onStart() called")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG,"SignupActivity - onResume() called")
+        Log.d(TAG,"LoginActivity - onResume() called")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG,"SignupActivity - onPause() called")
+        Log.d(TAG,"LoginActivity - onPause() called")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG,"SignupActivity - onStop() called")
+        Log.d(TAG,"LoginActivity - onStop() called")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,"SignupActivity - onDestroy() called")
+        Log.d(TAG,"LoginActivity - onDestroy() called")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(TAG,"SingupActivity - onRestart() called")
+        Log.d(TAG,"LoginActivity - onRestart() called")
+    }
+
+    fun onSignupButtonClicked(view: View){
+        Log.d(TAG,"LoginActivity - onSignupButtonClicked() called")
+
+        val intent = Intent(this, SignupActivity::class.java)
+        startActivity(intent)
     }
 
     fun onBackButtonClicked(view: View){
-        Log.d(TAG,"SignupActivity - onBackButtonClicked() called")
+        Log.d(TAG,"LoginActivity - onBackButtonClicked() called")
         finish()
     }
-
-
 }
