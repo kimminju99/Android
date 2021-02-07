f16d89100240030c79dd0958ebeb16f1a768eb45
kimminju99 <50831854+kimminju99@users.noreply.github.com>
2021-02-07 19:02:19 +0900

    자동 로그인 기능 구현을 위해 data 저장
    shared preference account에 저장

--- Lifecycle_meditation/Activity/SignupActivity.kt
+++ Lifecycle_meditation/Activity/SignupActivity.kt
@@ -42,6 +42,8 @@ class SignupActivity : AppCompatActivity() {
             onPolicyButtonClicked(view)
         }
 
+
+
     }
 
     override fun onStart() {
@@ -69,22 +71,28 @@ class SignupActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG,"SignupActivity - onDestroy() called")
+
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(TAG,"SingupActivity - onRestart() called")
-
-
-        val pref = getSharedPreferences("pref", 0)
+        // policyCheck로부터 Boolean값 받아오기
+        val pref = getSharedPreferences("policyCheck", 0)
         val checking = pref.getBoolean("checked",false)
 
-        Log.v(TAG," - checking : "+checking)
+        Log.v(TAG," SingupActivity - onRestart() - checkbox : "+checking)
 
+        // agree했으면
         if(checking){
+            // 체크 박스 체크
             binding.readPolicyCheckbox.isChecked = true
-        }
+            // 로그인 버튼 클릭 가능하게
+            binding.startbtnSignup.setOnClickListener {
+                onStartButtonClicked(binding.root)
+            }
+        } // disagree 했으면
         else{
+            // 체크박스 해제
             binding.readPolicyCheckbox.isChecked= false
         }
 
@@ -102,5 +110,25 @@ class SignupActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun onStartButtonClicked(view: View){
+        Log.d(TAG,"SignupActivity - onStartButtonClicked() called")
+
+        //자동 로그인을 위해 account에 데이터 저장
+        saveData()
+
+        intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun saveData(){
+        val account = getSharedPreferences("account",0)
+        val edit = account.edit()
+        edit.putString("name", binding.editTextTextPersonName.text.toString())
+        edit.putString("email", binding.editTextTextEmailAddress.text.toString())
+        edit.putString("password",binding.editTextTextPassword.text.toString())
+        edit.apply() // 값 저장
+
+    }
 
 }
