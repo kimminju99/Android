cbbf1fa415b71d1a101827424638c2bd11a3ce94
kimminju99 <50831854+kimminju99@users.noreply.github.com>
2021-02-15 00:59:30 +0900

    musicItem 오른쪽 버튼 클릭 가능하게

--- ListView/java/MusicItmAdaptor.kt
+++ ListView/java/MusicItmAdaptor.kt
@@ -1,14 +1,22 @@
 package com.example.listview
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.service.autofill.OnClickAction
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.example.listview.databinding.PlaylistItemBinding
 
 
-class MusicItmAdaptor(val context: Context, val MusicList : ArrayList<MusicItm>): BaseAdapter() {
+class MusicItmAdaptor(val context: Context, val MusicList:ArrayList<MusicItm>): BaseAdapter() {
+
+    val TAG: String = "로그"
 
     private val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     private lateinit var binding: PlaylistItemBinding
@@ -16,6 +24,7 @@ class MusicItmAdaptor(val context: Context, val MusicList : ArrayList<MusicItm>)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         binding = PlaylistItemBinding.inflate(inflater,parent,false)
 
+        // MusicList 받아오기
         val title = binding.tvTitlePlayList
         val time = binding.tvNamePlayList
 
@@ -23,6 +32,17 @@ class MusicItmAdaptor(val context: Context, val MusicList : ArrayList<MusicItm>)
 
         title.text = music.title
         time.text = music.name
+
+
+
+        // playList 클릭 가능케
+        binding.btnDotPlayList.setOnClickListener {
+            Toast.makeText(context,"Dot Button",Toast.LENGTH_SHORT).show()
+            Log.d(TAG,"MusicItmAdaptor - getView() called")
+
+        }
+
+
 
         return binding.root
     }
