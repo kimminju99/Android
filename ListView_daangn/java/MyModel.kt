0551aaaddcb237f97e44944222f98a70d1dfd189
kimminju99 <50831854+kimminju99@users.noreply.github.com>
2021-02-17 04:12:04 +0900

    Create MyModel.kt

---
+++ ListView_daangn/java/MyModel.kt
@@ -0,0 +1,13 @@
+package com.example.listview_daangn
+
+import android.util.Log
+
+data class MyModel(var image:Int, var title:String?, var place:String?, var price : String?) {
+    val TAG: String = "로그"
+    
+    // 기본 생성자
+    init {
+
+    }
+
+}
