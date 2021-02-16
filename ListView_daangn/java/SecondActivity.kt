fed3cadf8260201877a891fb6e279ff5da34b731
kimminju99 <50831854+kimminju99@users.noreply.github.com>
2021-02-17 04:11:40 +0900

    Create SecondActivity.kt

---
+++ ListView_daangn/java/SecondActivity.kt
@@ -0,0 +1,42 @@
+package com.example.listview_daangn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.listview_daangn.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    val TAG: String = "로그"
+    private lateinit var binding:ActivitySecondBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"SecondActivity - onCreate() called")
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // 데이터 받아오기
+        var readData = intent.extras
+        var image = readData?.getInt("content_image",0)
+        var title = readData?.getString("content_title","")
+        var place = readData?.getString("content_place","")
+        var price = readData?.getString("content_price","")
+        if (image != null) {
+            binding.imageViewSecond.setImageResource(image)
+        }
+        binding.tvTitleSecond.setText(title)
+        binding.tvPlaceSecond.setText(place)
+        binding.tvPriceSecond.setText(price)
+
+        // 뒤로가기
+        binding.btnBack.setOnClickListener {
+            Log.d(TAG,"SecondActivity - back button clicked")
+            finish()
+        }
+    }
+}
